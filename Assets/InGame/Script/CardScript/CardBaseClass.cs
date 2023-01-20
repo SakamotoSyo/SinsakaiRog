e757d1e3b9020230a9137abb236c7e29e9f9ad4d
sakamotosyo <syo10150000@gmail.com>
2023-01-20 23:01:04 +0900

    [Add]CardBaseClassに新たなパラメーターの追加

--- Assets/InGame/Script/CardScript/CardBaseClass.cs
+++ Assets/InGame/Script/CardScript/CardBaseClass.cs
@@ -2,25 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CardBaseClass
+[System.Serializable]
+public class CardBaseClass
 {
-    private float _cardPower;
+    private int _id;
+    private float _mainEffectPower;
+    private float _subEffectPower;
     private float _cardDefence;
     private float _cardCost;
+    public string Tartget => _target;
+    private string _target;
+    [Tooltip("カードに関する説明")]
+    private string _cardDescription;
+
+    private PlayerStatus _playerStatus;
+    private EnemyStaus _enemyStatus;
 
     [SerializeReference, SubclassSelector]
-    ICardEffect _effect;
+    List<ICardEffect> _effect = new List<ICardEffect>();
 
 
-    public void Init(float power,float defence, float cost) 
+    public CardBaseClass(int id, float mainPower,float subPower, float defence, float cost, string target, string Description)
     {
-        _cardPower = power;
+        _id = id;
+        _mainEffectPower = mainPower;
+        _subEffectPower = subPower;
         _cardDefence = defence;
         _cardCost = cost;
+        _target = target;
+        _cardDescription = Description;
     }
 
-    public void UseEffect() 
+    /// <summary>
+    /// 設定されたカードの効果を使う
+    /// </summary>
+    public void UseEffect(PlayerStatus player, EnemyStaus enemy) 
     {
-        _effect.UseEffect();
+        _playerStatus = player;
+        _enemyStatus = enemy;
+        //カードに設定した効果を順に発動
+        for (int i = 0; i < _effect.Count; i++) 
+        {
+            _effect[i].UseEffect();
+        }
     }
 }
