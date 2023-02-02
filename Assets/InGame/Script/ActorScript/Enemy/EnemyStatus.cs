4d885044f3882060531680caac833fc6e443799b
sakamotosyo <syo10150000@gmail.com>
2023-02-02 18:02:55 +0900

    [Fix]機能の分散をさせた

--- Assets/InGame/Script/ActorScript/Enemy/EnemyStatus.cs
+++ Assets/InGame/Script/ActorScript/Enemy/EnemyStatus.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[Serializable]
 public class EnemyStatus : StatusModelBase
 {
     [Tooltip("行動する回数")]
-    private int _actionNum = 0;
+    private int _actionNum = 1;
     //TODO: データの注入方法を考える現状だとSerializeField
+    [Tooltip("行動パターン")]
     public List<EnemyEffectData> EnemyStatusList => _effectDataList;
     [SerializeField]private List<EnemyEffectData> _effectDataList = new List<EnemyEffectData>();
     public List<EnemyEffectData> EnemyTurnEffect => _enemyTurnEffect;
@@ -15,36 +18,25 @@ public class EnemyStatus : StatusModelBase
     /// <summary>
     /// このターンの行動を決定する
     /// </summary>
-    public void AttackDecision() 
+    public void AttackDecision(IReceiveEnemyEffect enemyEffect) 
     {
         for (int i = 0; i < _actionNum; i++) 
         {
-            _enemyTurnEffect.Add(_effectDataList[Random.Range(0, _effectDataList.Count)]);
-        }
-    }
-
-    /// <summary>
-    /// 行動を開始する
-    /// </summary>
-    /// <param name="playerStatus"></param>
-    /// <param name="enemyStatus"></param>
-    public void AttackEffect(PlayerStatus playerStatus, EnemyStatus enemyStatus) 
-    {
-        for (int i = 0; i < _enemyTurnEffect.Count; i++) 
-        {
-            _enemyTurnEffect[i].EnemyEffect.UseEffect(playerStatus, enemyStatus, _enemyTurnEffect[i].Attack, _enemyTurnEffect[i].Target);
+            _enemyTurnEffect.Add(_effectDataList[UnityEngine.Random.Range(0, _effectDataList.Count)]);
         }
     }
 }
 
+[System.Serializable]
 public struct EnemyEffectData
 {
     public IEffect EnemyEffect => _enemyEffect;
+    [SubclassSelector, SerializeReference]
     private IEffect _enemyEffect;
 
     public TargetType Target => _targetType;
-    private TargetType _targetType;
+    [SerializeField]private TargetType _targetType;
 
-    public float Attack => _attack;
-    private float _attack;
+    public float EffectPower => _effectPower;
+    [SerializeField]private float _effectPower;
 }
