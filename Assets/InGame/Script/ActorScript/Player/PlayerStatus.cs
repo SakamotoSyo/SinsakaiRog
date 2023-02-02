0470dfb513a362920f77881f993e957d499bc308
sakamotosyo <syo10150000@gmail.com>
2023-02-02 18:02:15 +0900

    [Update]Costに関する処理の追加

--- Assets/InGame/Script/ActorScript/Player/PlayerStatus.cs
+++ Assets/InGame/Script/ActorScript/Player/PlayerStatus.cs
@@ -1,42 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using System;
 
+
+[Serializable]
 public class PlayerStatus : StatusModelBase, IPlayerStatus
 {
-    [Header("カードに使用に使うコスト")]
-    [SerializeField] private float _myCost;
+    public float Cost => _myCost.Value;
+    public IObservable<float> CostOb => _myCost; 
+    public IReactiveCollection<CardBaseClass> HandCardList => _handCardList;
+    public List<CardBaseClass> DeckCardList => _deckCardList;
 
+    [Header("カードに使用の際に必要なコスト")]
+    [SerializeField] private ReactiveProperty<float> _myCost;
     [Tooltip("手札のカードリスト")]
-   public IReactiveCollection<CardBaseClass> HandCardList => _handCardList;
-   private ReactiveCollection<CardBaseClass> _handCardList = new ReactiveCollection<CardBaseClass>();
+    private ReactiveCollection<CardBaseClass> _handCardList = new ReactiveCollection<CardBaseClass>();
+    [Tooltip("山札のカードリスト")]
+    [SerializeField] private List<CardBaseClass> _deckCardList = new List<CardBaseClass>();
 
-   [Tooltip("山札のカードリスト")]
-   public List<CardBaseClass> DeckCardList => _deckCardList;
-   [SerializeField]private List<CardBaseClass> _deckCardList = new List<CardBaseClass>();
+    /// <summary>
+    /// コストを使用する
+    /// </summary>
+    /// <param name="useCost"></param>
+    public void UseCost(float useCost) 
+    {
+        _myCost.Value -= useCost;
+        Debug.Log(_myCost);
+    }
 
     /// <summary>
     /// DataBaseからランダムにカードを追加する
     /// </summary>
     /// <param name="value"></param>
-    public void TestAddCardList(int value) 
+    public void TestAddCardList()
     {
-        for (int i = 0; i < value; i++) 
-        {
-            //_deckCardList.Add();
-        }
+        _deckCardList.Add(DataBaseScript.CardBaseClassList[0]);     
     }
 
     //TODO:ここのRemoveコメントを外す
     /// <summary>
     /// カードをドローする
     /// </summary>
-    public void DrowCard() 
+    public void DrowCard()
     {
         _handCardList.Add(_deckCardList[0]);
         //_deckCardList.RemoveAt(0);
     }
-
-
 }
