4a70fe9f9e25302a84d8fd3dc8664f125554e293
sakamotosyo <syo10150000@gmail.com>
2023-02-03 03:57:11 +0900

    [Add]PlayerとEnemyのAnimationを管理するScript作成

---
+++ Assets/InGame/Script/ActorScript/Enemy/EnemyAnimaiton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class EnemyAnimaiton
+{
+    [AnimationParameter]
+    [SerializeField] private string _damageParm; 
+    [AnimationParameter]
+    [SerializeField] private string _attackParm;
+    [SerializeField] private Animator _enemyAnim;
+    [SerializeField] private Animator _effectAnim;
+
+    /// <summary>
+    /// ダメージAnimationをスタートする
+    /// </summary>
+    public void DamageAnim() 
+    {
+        _enemyAnim.SetTrigger(_damageParm);
+        _effectAnim.SetTrigger("AttackEffect");
+    }
+
+    public void AttackAnim() 
+    {
+        _enemyAnim.SetTrigger(_attackParm);
+    }
+}
---
+++ Assets/InGame/Script/ActorScript/Player/PlayerAnim.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PlayerAnim
+{
+    [AnimationParameter]
+    [SerializeField] private string _downParm;
+    [AnimationParameter]
+    [SerializeField] private string _attackParm;
+    [SerializeField] private Animator _playerAnim;
+
+    public void DamageAnim() 
+    {
+        _playerAnim.SetTrigger(_downParm);
+    }
+
+    public void AttackAnim() 
+    {
+        _playerAnim.SetTrigger(_attackParm);
+    }
+}
