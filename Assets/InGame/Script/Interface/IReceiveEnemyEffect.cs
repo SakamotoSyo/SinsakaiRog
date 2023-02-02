64067fd954bf8ff939907ab65bcf8c5b8c1bde52
sakamotosyo <syo10150000@gmail.com>
2023-02-02 17:53:19 +0900

    [Add]IDamagebleに代わるインターフェースの作成

---
+++ Assets/InGame/Script/Interface/IReceiveEnemyEffect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReceiveEnemyEffect
+{
+    public void AddDamage(float damage);
+    /// <summary>
+    /// ‚±‚Ìƒ^[ƒ“‚Ìs“®‚ğŒˆ’è‚µ‚ÄUŒ‚‚·‚é
+    /// </summary>
+    public void Attack();
+}
---
+++ Assets/InGame/Script/Interface/IReceivePlayerEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReceivePlayerEffect
+{
+    /// <summary>
+    /// ƒ_ƒ[ƒW‚ğó‚¯‚éˆ—
+    /// </summary>
+    /// <param name="damage"></param>
+    public void AddDamage(float damage);
+
+    public void DrowCard();
+}
