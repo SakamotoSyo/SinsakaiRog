1c18ece3bfacdcede42371ed9b786ac35515738b
sakamotosyo <syo10150000@gmail.com>
2023-02-04 02:56:53 +0900

    [Add]Draw時のAniamtionを追加

--- Assets/InGame/Script/CardScript/CardAnimation.cs
+++ Assets/InGame/Script/CardScript/CardAnimation.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DG.Tweening;
 
 [Serializable]
 public class CardAnimation 
 {
-    [SerializeField] Animator _anim;
+    [SerializeField] private Animator _anim;
     [AnimationParameter]
-    [SerializeField] string _selectParm;
+    [SerializeField] private string _selectParm;
+    [Tooltip("ƒhƒ[‚µ‚½Û‚ÌAnimation‚ÌPosition‚ğ•Ï‚¦‚ê‚é")]
+    [SerializeField] private Vector3 _drawAnimOffSet;
+    [SerializeField] private CardEvent _cardEvent;
+    private Transform _parentTransform;
 
     public void Init(Animator anim) 
     {
@@ -22,5 +25,23 @@ public class CardAnimation
     public void SelectAnim(bool play) 
     {
         _anim.SetBool(_selectParm, play);
+    }
+
+    /// <summary>
+    /// ¶¬‚³‚ê‚éêŠ‚ÌTransform
+    /// </summary>
+    /// <param name="transform"></param>
+    public void SetParentTransform(Transform transform) 
+    {
+        _parentTransform = transform;
+    }
+
+    public void DrawAnim(Transform transform) 
+    {
+        DOTween.To(() => transform.transform.localPosition - _parentTransform.localPosition - _drawAnimOffSet,
+            x => transform.transform.localPosition = x,
+            transform.transform.localPosition, 0.5f)
+            .OnStart(() => _cardEvent.enabled = false)
+            .OnComplete(() => _cardEvent.enabled = true);
     }
 }
