85d644cde74b53b3977b21ea3766977a9a44e48e
sakamotosyo <syo10150000@gmail.com>
2023-02-04 02:55:29 +0900

    [Add]Enemyの攻撃に関するScriptを作成

---
+++ Assets/InGame/Script/ActorScript/Enemy/EnemyAttack.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack
+{
+    /// <summary>
+    /// çsìÆÇäJénÇ∑ÇÈ
+    /// </summary>
+    /// <param name="playerStatus"></param>
+    /// <param name="enemyStatus"></param>
+    public void AttackEffect(PlayerController playCon,EnemyController enemyCon, EnemyEffectData enemyTurnEffect)
+    {
+        enemyTurnEffect.EnemyEffect.UseEffect(playCon, enemyCon, enemyTurnEffect.EffectPower, enemyTurnEffect.Target);
+    }
+}
