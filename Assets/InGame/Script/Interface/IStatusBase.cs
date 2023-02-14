d26def72b5f42eceb682bf1e24485fe8eefd9a57
sakamotosyo <syo10150000@gmail.com>
2023-02-14 19:33:15 +0900

    [Add]Statusの基底クラス用のInterfaceを追加

---
+++ Assets/InGame/Script/Interface/IStatusBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+public interface IStatusBase
+{
+    public IObservable<float> GetDefeceOb();
+    public IObservable<float> GetMaxHpOb();
+    public IObservable<float> GetCurrentHpOb();
+    public float GetDefenceNum();
+    public float GetMaxHpNum();
+    public float GetCurrentHpNum();
+    public void AddDamage(float damage);
+    public void Healing(float damage);
+    public void DefenseIncrease(float num);
+    public bool DownJudge(float damage);
+
+}
