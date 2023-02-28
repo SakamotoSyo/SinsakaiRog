c7e8be8bd0b9af99c939457cc396028d661c5f78
sakamotosyo <syo10150000@gmail.com>
2023-02-28 10:45:55 +0900

    [Update]効果音を追加。確率で異なったシーンへ移動するように

--- Assets/InGame/Script/DownTheStairsScript.cs
+++ Assets/InGame/Script/DownTheStairsScript.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System;
 
 public class DownTheStairsScript : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
     async void Start()
     {
         await FadeScript.Instance.FadeIn();
+        _audioSource.Play();
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
         GameManager.NextCurrentLevel();
-        FadeScript.Instance.FadeOut();
-        var num = Random.Range(0, 100);
-        if (num < 50)
+        var num = UnityEngine.Random.Range(0, 100);
+        if (num < 40)
         {
             LoadSceneManager.ToEventScene();
         }
