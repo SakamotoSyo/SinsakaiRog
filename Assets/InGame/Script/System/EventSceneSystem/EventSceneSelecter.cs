ded1af33f11fa3c3d4c6f246c6023bdcdee6df1d
sakamotosyo <syo10150000@gmail.com>
2023-02-28 10:59:14 +0900

    [Update]新たにショップに関するイベントの追加

--- Assets/InGame/Script/System/EventSceneSystem/EventSceneSelecter.cs
+++ Assets/InGame/Script/System/EventSceneSystem/EventSceneSelecter.cs
@@ -11,25 +11,50 @@ public class EventSceneSelecter : MonoBehaviour
     [SerializeField] private Button _notSelectButton;
     [SerializeField] private EventDataBase _eventData;
     [SerializeField] private Text _eventDescriptionText;
+    [SerializeField] private GameObject _eventObj;
     [SceneName]
     [SerializeField] private string _nextSceneName;
+    [SerializeField] private ShopScript _shopScript;
+    [Header("ショップがどの程度出現するか")]
+    [SerializeField] private int _shopAppears;
+    [SerializeField] private IPlayerStatus _playerStatus;
     private void Start()
     {
-        EventSelect();
+        _playerStatus = PlayerEventPresenter.PlayerStatus;
+        //セーブされているデータをセットする
+        _playerStatus.SetPlayerSaveData(GameManager.SaveData);
+        var num = UnityEngine.Random.Range(0, 101);
+        if (num < _shopAppears)
+        {
+            _shopScript.OpenShop();
+        }
+        else 
+        {
+            EventSelect();
+        }
+
+
     }
 
     public async void EventSelect() 
     {
-        //セーブされているデータをセットする
-        PlayerEventPresenter.PlayerStatus.SetPlayerSaveData(GameManager.SaveData);
+        _eventObj.SetActive(true);
         var Event = _eventData.RamdomEventData();
         _eventDescriptionText.text = Event.EventDescription;
         await FadeScript.Instance.FadeIn();
         //それぞれにイベントを登録
-        _selectButton.onClick.AddListener(() => Event.EventEffect.IfSelected());
+        _selectButton.onClick.AddListener(() => Event.EventEffect.IfSelected(_eventDescriptionText, Event));
         _selectButton.onClick.AddListener(() =>  NextScene());
         _notSelectButton.onClick.AddListener(() => Event.EventEffect.IfNotSelected());
         _notSelectButton.onClick.AddListener(() => NextScene());
+    }
+
+    /// <summary>
+    /// ショップを開く
+    /// </summary>
+    public void OpenShop() 
+    {
+        
     }
 
     public void SetDescription() 
@@ -43,7 +68,7 @@ public class EventSceneSelecter : MonoBehaviour
         _notSelectButton.enabled = false;
         await UniTask.Delay(TimeSpan.FromSeconds(1f));
         Debug.Log("次のシーンへ");
-        GameManager.SavePlayerData(PlayerEventPresenter.PlayerStatus.GetPlayerSaveData());
+        GameManager.SavePlayerData(_playerStatus.GetPlayerSaveData());
         SceneManager.LoadScene(_nextSceneName);
     }
 }
